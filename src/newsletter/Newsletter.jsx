8f130e333c114a15086d4a4294cd41348cc41176
Abbas Sher <57453785+Abbassher55@users.noreply.github.com>
2024-03-26 01:41:52 +0500

    update

--- src/newsletter/Newsletter.jsx
+++ src/newsletter/Newsletter.jsx
@@ -6,7 +6,7 @@ export default function Newsletter() {
           <div className="flex flex-col items-center justify-center space-y-6 md:w-1/2">
             <p className="text-center text-sm font-medium tracking-[.4rem]">35,000+ ALREADY JOINED</p>
             <h2 className="max-w-sm text-center text-3xl font-medium leading-10">Stay up-to-date with what we’re doing</h2>
-            <form action="">
+            <form>
               <div className="flex gap-2 max-sm:flex-wrap">
                 <label htmlFor="email" className="opacity-0">Email</label>
                 <input type="email" id="email" className="w-full rounded-md px-2 text-custom-dark focus-visible:border focus-visible:border-custom-grey focus-visible:outline-none max-sm:py-3 md:min-w-80" required />
